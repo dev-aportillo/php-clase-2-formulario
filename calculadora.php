cdc6ed4e2cda5547c91c911383195e10188ef393
dev-aportillo <152889962+dev-aportillo@users.noreply.github.com>
2023-12-04 11:39:38 -0300

    Add files via upload

---
+++ calculadora.php
@@ -0,0 +1,16 @@
+<body>
+    <input type="text" id="numero1" placeholder="Número 1"/>
+    <input type="text" id="numero2" placeholder="Número 2"/>
+    <input type="text" id="resultado" placeholder="Resultado"/>
+    
+    <br><br>
+    
+    <button id="sumar">+</button>
+    <button id="restar">-</button>
+    <button class="operador">*</button>
+    <button class="operador">x<sup>y</sup></button>
+    
+    <br><br>
+    
+    <button id="total">=</button>
+</body>
